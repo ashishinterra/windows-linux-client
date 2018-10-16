2a0db204d5abcec1bb4a5aed8a94d433a9952055
Andrei Korostelev <andrei.korostelev@sioux.eu>
2018-10-16 13:20:22 +0200

    Update from the upstream

--- Software/Client/TestProjects/testReseptInstaller/linux/worker_start_build_test.sh
+++ Software/Client/TestProjects/testReseptInstaller/linux/worker_start_build_test.sh
@@ -64,16 +64,22 @@ function run_test()
         if [[ "${ccpy_state}" == "OK" ]]; then
             local osspec=$(lsb_release --id --short | tr "[:upper:]" "[:lower:]")$(lsb_release --release --short | egrep -o [0-9]+ | sed -n '1p')-x64
             local client_version=$(cut -d '=' -f 2 ${REPO_DIR}/Software/Client/version)
-            local installer_package=KeyTalkClient-${client_version}-${osspec}.tgz
-            local installer_package_path="${REPO_DIR}/Software/Client/Projects/Export/${installer_package}"
-            echo "Collecting installer package ${installer_package}"
-            if [ -f "${installer_package_path}" ]; then
-                cp "${installer_package_path}" "${RESULT_DIR}/"
-                return 0
-            else
-                echo "ERROR: No installer package found under ${installer_package_path}" >&2
-                return 1
-            fi
+            local app_package=KeyTalkClient-${client_version}-${osspec}.tgz
+            local python_demo_package=KeyTalkClient-${client_version}.python.demo.tgz
+            local apache_remote_deployment_package=KeyTalkClient-${client_version}.linux.apache-remote-deployment.tgz
+            local tomcat_remote_deployment_package=KeyTalkClient-${client_version}.linux.tomcat-remote-deployment.tgz
+
+            for package in ${app_package} ${python_demo_package} ${apache_remote_deployment_package} ${tomcat_remote_deployment_package} ; do
+                echo "Collecting installer package ${package}"
+                local installer_package_path="${REPO_DIR}/Software/Client/Projects/Export/${package}"
+                if [ -f "${installer_package_path}" ]; then
+                    cp "${installer_package_path}" "${RESULT_DIR}/"
+                    return 0
+                else
+                    echo "ERROR: No installer package found under ${installer_package_path}" >&2
+                    return 1
+                fi
+            done
         else
             echo "ERROR: Test failed" >&2
             return 1
