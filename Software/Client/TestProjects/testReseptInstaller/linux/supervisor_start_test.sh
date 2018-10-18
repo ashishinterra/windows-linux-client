18aa7a6cf225c1f707823c242031df15eba4e3cb
Andrei Korostelev <andrei.korostelev@sioux.eu>
2018-10-18 11:36:50 +0200

    Fix script typo

--- Software/Client/TestProjects/testReseptInstaller/linux/supervisor_start_test.sh
+++ Software/Client/TestProjects/testReseptInstaller/linux/supervisor_start_test.sh
@@ -18,7 +18,7 @@ FAKE_BUILD_TEST=false
 # dict( build image name -> installation image name )
 TEST_IMAGES=\
 " debian-8-keytalk-build-test:debian-8-keytalk-install-test"\
-" debian-9-keytalk-build-test:debian-9-keytalk-install-test" \
+" debian-9-keytalk-build-test:debian-9-keytalk-install-test"\
 " ubuntu-16.04-keytalk-build-test:ubuntu-16.04-keytalk-install-test"
 
 # Repository root, the supervisor shares it with the workers.
